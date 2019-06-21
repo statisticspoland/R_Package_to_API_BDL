6943bb1084cc93c0ace9e9f3b344ab0b2de8d222
Krzysztof Kania <kontakt@krzysztofkania.com>
2019-06-21 12:59:46 +0200

    Fix link in docs

--- R/bdl.R
+++ R/bdl.R
@@ -4,7 +4,7 @@
 #' @section Package options:
 #' \describe{
 #' \item{`bdl.api_private_key`}{String with BDL API key which you can get
-#' \link{https://bdl.stat.gov.pl/api/v1/client?theme=Default} 
+#' \url{https://bdl.stat.gov.pl/api/v1/client?theme=Default} 
 #' Example: \code{options(bdl.api_private_key = "3sd1dea1-f89d-488c-c441-08d64fbd20e9")}}
 #' 
 #' }
@@ -13,6 +13,5 @@
 #' @docType package
 #' @import methods
 NULL
-
 # global var note fix
 if(getRversion() >= "2.15.1") utils::globalVariables(c("val"))
